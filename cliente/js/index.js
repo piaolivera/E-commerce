59a97008bb8f6af61bbb1f7c82c3a65617cab474
piaolivera <piaoliveraa@gmail.com>
2023-08-31 14:59:32 -0300

    --

--- cliente/js/index.js
+++ cliente/js/index.js
@@ -1,15 +1,30 @@
 const shopContent = document.getElementById("shopContent");
+const cart = []; // este es nuestro carrito. un array vacio
 
-productos.forEach((products) => {
-    const content = document.createElement("div");
-    content.innerHTML = `
-    <img src="${produtos.img}">
-    <h3>${productos.productName} </h3>
-    <p>${productos.price} $</p>
-    `;
-    shopContent.append(content);
-    
+productos.forEach((product) => {
+  const content = document.createElement("div");
+  content.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.productName}</h3>
+    <p>${product.price} $</p>
+  `;
+  content.className = "card-products";
+  shopContent.appendChild(content);
 
-    
-    
+  const buyButton = document.createElement("button");
+  buyButton.innerText = "Comprar";
+
+  content.append(buyButton);
+
+  buyButton.addEventListener("click", ()=>{
+    cart.push({ // atraves de un push agregamos al carrito
+        id: product.id,
+        productName: product.productName,
+        price: product.price,
+        quanty: product.quanty,
+        img: product.img,
+
+    })
+    console.log(cart)
+  })
 });
